7617b4ad89322655f3aba6305656f6a1f71ac421
slx-world <slx9920@outlook.com>
2024-11-26 12:51:55 +0800

    自定义SQL

--- src/main/java/com/itheima/mp/mapper/UserMapper.java
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -1,10 +1,15 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper extends BaseMapper<User> {
 
     User queryById(Long id);
 
+    @Select("update user set balance = balance + #{money} ${ew.customSqlSegment}")
+    void deductBalanceByIds(@Param("money") int money, @Param("ew")QueryWrapper<User> wrapper);
 }
--- src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
@@ -91,6 +92,7 @@ public class UserMapperTest {
         // 1. 构建查询条件 where name = "Jack"
         QueryWrapper<User> wrapper = new QueryWrapper<User>()
                 .eq("username", "Jack");
+
         // 2. 更新数据，user 中非 null 的字段都会作为 set 语句
         User user = new User();
         user.setBalance(2000);
@@ -104,12 +106,38 @@ public class UserMapperTest {
 
         // 1. 生成 SQL
         UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
-                .setSql("balance = balance + 100") // 注意，这里 balance + 100 是一个 SQL 片段
+                .setSql("balance = balance - 200") // 注意，这里 balance - 200 是一个 SQL 片段
                 .in("id", ids);// 注意，这里 in 中的参数是 Long 类型
 
         // 2. 更新，注意第一个参数可以给 null，也就是不填更新字段和数据
         // 而是基于 UpdateWrapper 中的 setSql 来更新
         int i = userMapper.update(null, wrapper);
         System.out.println("i = " + i);
+    }
+
+    @Test
+    void testLambdaQueryWrapper() {
+        // 1. 构建条件 where username like '%o%' and balance >= 1000
+        LambdaQueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .lambda()
+                .select(User::getId, User::getUsername, User::getInfo, User::getBalance)
+                .like(User::getUsername, "o")
+                .ge(User::getBalance, 1000);
+
+        // 2. 查询数据
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void testCustomWrapper() {
+        // 1. 准备自定义查询条件
+        List<Long> ids = Arrays.asList(1L, 2L, 4L);
+
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .in("id", ids);
+
+        // 2. 调用 mapper 的自定义方法，直接传递 wrapper
+        userMapper.deductBalanceByIds(500, wrapper);
     }
 }
