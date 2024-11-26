2c5aa1e20fbb0557cb2f24742600ddeb95db8e88
slx-world <slx9920@outlook.com>
2024-11-26 09:15:29 +0800

    first commit

---
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.itheima.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.mp.domain.po.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
