41f20244deb942074c1e567679f177dc0b9eadc3
slx-world <slx9920@outlook.com>
2024-11-26 19:21:00 +0800

    Lambda

--- src/test/java/com/itheima/mp/service/UserServiceTest.java
+++ src/test/java/com/itheima/mp/service/UserServiceTest.java
@@ -68,4 +68,67 @@ public class UserServiceTest {
         long e = System.currentTimeMillis();
         System.out.println("耗时：" + (e - b));
     }
+
+    @Test
+    void testLambdaQuery() {
+        // 1. 查询 1 个
+        User rose = userService.lambdaQuery()
+                .eq(User::getUsername, "Rose")
+                .one(); // .one() 查询 1 个
+
+        System.out.println(rose);
+
+        // 2. 查询多个
+        List<User> list = userService.lambdaQuery()
+                .like(User::getUsername, "o")
+                .list();// .list() 查询集合
+
+        list.forEach(System.out::println);
+
+        // 3. count 统计
+        Long count = userService.lambdaQuery()
+                .like(User::getUsername, "o")
+                .count(); // .count() 统计
+
+        System.out.println("count = " + count);
+    }
+
+    @Test
+    void testQueryUser() {
+        List<User> users = queryUser("o", 1, null, null);
+        users.forEach(System.out::println);
+    }
+
+    private List<User> queryUser(String username, Integer status, Integer minBalance, Integer maxBalance) {
+        return userService.lambdaQuery()
+                .like(username != null, User::getUsername, username)
+                .eq(status != null, User::getStatus, status)
+                .ge(minBalance != null, User::getBalance, minBalance)
+                .le(maxBalance != null, User::getBalance, maxBalance)
+                .list();
+    }
+
+    @Test
+    void testLambdaUpdate() {
+        boolean i = userService.lambdaUpdate()
+                .set(User::getBalance, 800) // set balance = 800
+                .eq(User::getUsername, "Jack") // where username = 'Jack'
+                .update();// 执行 Update
+
+        System.out.println("i = " + i);
+    }
+
+    @Test
+    void testUpdateBalance() {
+        updateBalance(0L, 1L, null);
+    }
+
+    private void updateBalance(Long balance, Long id, String username) {
+        userService.lambdaUpdate()
+                .set(User::getBalance, balance)
+                .set(balance == 0, User::getStatus, 2)
+                .eq(id != null, User::getId, id)
+                .eq(username != null, User::getUsername, username)
+                .update();
+    }
 }
